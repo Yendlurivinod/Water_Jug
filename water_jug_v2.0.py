2c65f4adf1cd2e2a563051c48b38b76c03acc7ba
adr485 <66263972+adr485@users.noreply.github.com>
2023-02-14 10:37:13 +0530

    Add files via upload
    updated version for any sizes

---
+++ water_jug_v2.0.py
@@ -0,0 +1,48 @@
+def fill(j,c,c1):
+    if c1!=0:
+        j=j+c1
+        if j<=c:
+            return j,0
+        else:
+            return c,(j-c)
+    else:
+        j=c
+        return j
+def empty(l):
+    return 0
+
+
+l=[0,0]
+c=list(map(int,input().split()))
+rs=int(input())
+while(1):
+    if l[0]==0 and l[1]==0:
+        l[0]=fill(l[0],c[0],0)
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
+    elif l[0]!=0 and l[1]==0:
+        l[1],l[0]=fill(l[1],c[1],l[0])
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
+    elif l[0]!=0 and l[1]==c[1]:
+        l[1]=empty(l[1])
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
+    elif l[0]!=0 and l[1]!=0:
+        l[1],l[0]=fill(l[1],c[1],l[0])
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
+    elif l[0]==0 and l[1]!=0:
+        l[0]=fill(l[0],c[0],0)
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
+    elif l[0]==c[0] and l[1]==c[1]:
+        l[1]=empty(l[1])
+        print(l)
+        if l[0]==rs or l[1]==rs:
+            break
