c926e520ec3b7a0e3361fecaa28687f9645320e9
adr485 <66263972+adr485@users.noreply.github.com>
2023-02-20 19:44:55 +0530

    Update water_jug_v2.0.py

--- water_jug_v2.0.py
+++ water_jug_v2.0.py
@@ -15,34 +15,37 @@ def empty(l):
 l=[0,0]
 c=list(map(int,input().split())) # read capacities of jugs
 rs=int(input())    # required amount of water
-while(1):
-    if l[0]==0 and l[1]==0:
-        l[0]=fill(l[0],c[0],0)
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
-    elif l[0]!=0 and l[1]==0:
-        l[1],l[0]=fill(l[1],c[1],l[0])
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
-    elif l[0]!=0 and l[1]==c[1]:
-        l[1]=empty(l[1])
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
-    elif l[0]!=0 and l[1]!=0:
-        l[1],l[0]=fill(l[1],c[1],l[0])
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
-    elif l[0]==0 and l[1]!=0:
-        l[0]=fill(l[0],c[0],0)
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
-    elif l[0]==c[0] and l[1]==c[1]:
-        l[1]=empty(l[1])
-        print(l)
-        if l[0]==rs or l[1]==rs:
-            break
+if c[0]%2==0 and c[1]%2==0 and rs%2==1:
+    print('NO SOLUTION')
+else:
+    while(1):
+        if l[0]==0 and l[1]==0:
+            l[0]=fill(l[0],c[0],0)
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
+        elif l[0]!=0 and l[1]==0:
+            l[1],l[0]=fill(l[1],c[1],l[0])
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
+        elif l[0]!=0 and l[1]==c[1]:
+            l[1]=empty(l[1])
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
+        elif l[0]!=0 and l[1]!=0:
+            l[1],l[0]=fill(l[1],c[1],l[0])
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
+        elif l[0]==0 and l[1]!=0:
+            l[0]=fill(l[0],c[0],0)
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
+        elif l[0]==c[0] and l[1]==c[1]:
+            l[1]=empty(l[1])
+            print(l)
+            if l[0]==rs or l[1]==rs:
+                break
